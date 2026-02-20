adbd5326c98d3a7e520f70a5658e4292a42976c7
hugomgg <hugomgg@gmail.com>
2026-02-20 18:54:47 -0300

    feat: integra entorno Docker con Nginx, MySQL y Vite

--- php.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-D:\PROGRAMAS\php\php.exe %*
--- vite.config.js
+++ vite.config.js
@@ -43,4 +43,15 @@ export default defineConfig({
             },
         },
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    }
 });
